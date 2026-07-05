d61e549ae4a94c2dd36e532d9f75e61ffc4f2656
Shella May Cuenca <shellamaycuenca12@gmail.com>
2026-07-05 23:48:48 +0800

    new

--- static/js/projectData.js
+++ static/js/projectData.js
@@ -4,7 +4,7 @@ var projects = [
         desc: `A modern product showcase inspired by Apple’s design language.
         This project highlights the iPad Air’s sleek aesthetics, powerful M3 chip,
         and Liquid Retina display through a clean, responsive, and visually polished layout.`,
-        img: "main_assets/img/apple_ipad_air.png",
+        img: "../../main_assets/img/apple_ipad_air.png",
         link: "https://she1028.github.io/Apple_iPad_Air/",
         githublink: "https://github.com/she1028/Apple_iPad_Air.git"
     },
@@ -13,7 +13,7 @@ var projects = [
         desc: `A travel-inspired website featuring the natural beauty of El Nido, Palawan.
         Designed with immersive visuals and clear content sections to promote tourism,
         destinations, and the overall travel experience.`,
-        img: "main_assets/img/elnido_palawan.png",
+        img: "../../main_assets/img/elnido_palawan.png",
         link: "https://she1028.github.io/ElNido_Palawan/",
         githublink:"https://github.com/she1028/ElNido_Palawan.git"
     },
@@ -22,7 +22,7 @@ var projects = [
         desc: `An elegant digital art gallery showcasing Renaissance-era masterpieces.
         Focused on visual presentation and historical appreciation, this project emphasizes
         layout balance, typography, and an engaging viewing experience.`,
-        img: "main_assets/img/renaissance_gallery.png",
+        img: "../../main_assets/img/renaissance_gallery.png",
         link: "https://she1028.github.io/Renaissance_Gallery/",
         githublink:"https://github.com/she1028/Renaissance_Gallery.git"
     },
@@ -31,7 +31,7 @@ var projects = [
         desc: `A dynamic recipe platform powered by API integration.
         Users can explore meal ideas, view recipe details, and discover new dishes,
         demonstrating practical use of APIs, data handling, and interactive UI design.`,
-        img: "main_assets/img/api_utilization.png",
+        img: "../../main_assets/img/api_utilization.png",
         link: "https://she1028.github.io/API_Utilization/",
         githublink:"https://github.com/she1028/API_Utilization.git"
     }
